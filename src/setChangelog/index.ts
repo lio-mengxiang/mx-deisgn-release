43f1bad7a1be2caa08166ca63b84e7f215d5629a
lio-mengxiang <1334196450@qq.com>
2022-03-24 23:27:42 +0800

    feat: add ts-node

--- src/setChangelog/index.ts
+++ src/setChangelog/index.ts
@@ -4,8 +4,7 @@ import { CHANGELOG_NAME } from "../config/constans";
 
 export const getOldLog = () => {
   const logPath = getProjectPath(CHANGELOG_NAME);
-  console.log('existsSync(logPath): ', existsSync(logPath), logPath);
-  if (existsSync(logPath)) {
+  if (!existsSync(logPath)) {
     fs.writeFileSync(logPath, "");
   }
   const oldFile = fs.readFileSync(logPath);
