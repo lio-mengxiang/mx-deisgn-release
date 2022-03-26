793fdac5a5ebbb9fd509e543f6c74d1aec5bfbf5
lio-mengxiang <1334196450@qq.com>
2022-03-26 22:03:52 +0800

    feat: init

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## 1.0.13 (2022-03-25)
+
+
+### Features
+
+* 2 ([a61a3c6](https://github.com/lio-mengxiang/mx-deisgn-release/commit/a61a3c62589879d53bee334ddd0f5de9dfcde223))
+* 2 ([4787e05](https://github.com/lio-mengxiang/mx-deisgn-release/commit/4787e055583d29b9873717ad73b77c8d920b484b))
+* 2 ([1404d9a](https://github.com/lio-mengxiang/mx-deisgn-release/commit/1404d9a6254fc3f36fa6bc6d699b59d59abfc60f))
+* 3 ([e049515](https://github.com/lio-mengxiang/mx-deisgn-release/commit/e04951523ac8a770052a46e7becdddf1feaceaf5))
+* 3 ([dd13420](https://github.com/lio-mengxiang/mx-deisgn-release/commit/dd1342031f697c2181ac2cc4ce4596eba2364e5c))
+* a ([d10f928](https://github.com/lio-mengxiang/mx-deisgn-release/commit/d10f928d126ae00cd0dcbcfe0fd8878033845a11))
+* d ([77e88fd](https://github.com/lio-mengxiang/mx-deisgn-release/commit/77e88fdf5ef2d23defc284de977b541057f41ee2))
+* init ([813c3eb](https://github.com/lio-mengxiang/mx-deisgn-release/commit/813c3ebdf5691191bac092dd9f8132e707caeb40))
+* s ([3840329](https://github.com/lio-mengxiang/mx-deisgn-release/commit/3840329f9d763978ab01604495a88981ceaeadd4))
+
+
+
---
+++ src/config/functions.ts
@@ -0,0 +1,59 @@
+import child_process from 'child_process';
+import chalk from 'chalk';
+import path from 'path';
+import fs from 'fs';
+import ora from 'ora';
+
+const spinner = ora()
+const execSync = child_process.execSync;
+
+export const run = (command: string) => {
+  try {
+    execSync(command, { cwd: process.cwd() });
+  } catch (error) {
+    process.exit(1);
+  }
+};
+
+export const timeLog = (logInfo: string, type: 'start' | 'end') => {
+  if (type === 'start') {
+    spinner.start( `${chalk.cyanBright(`task start(开始任务): ${logInfo}`)} \r\n`);
+  } else {
+    spinner.succeed(`${chalk.green(`task end(任务结束): ${logInfo}`)} \r\n`);
+  }
+};
+
+// 获取项目文件
+export const getProjectPath = (dir = './'): string => {
+  return path.join(process.cwd(), dir);
+};
+
+export function compose(middleware) {
+  const otherOptions = {};
+  function dispatch(index, otherOptions) {
+    if (index == middleware.length) return;
+    const currMiddleware = middleware[index];
+    currMiddleware((addOptions) => {
+      dispatch(++index, { ...otherOptions, ...addOptions });
+    }, otherOptions).catch((error) => {
+      console.log('💣 发布失败，失败原因：', error);
+    });
+  }
+  dispatch(0, otherOptions);
+}
+
+/**
+ * 获取当前package.json的版本号
+ */
+export const getOriginPackageJson = (): Record<string, any> => {
+  const packageJson = JSON.parse(fs.readFileSync(getProjectPath('package.json'), 'utf-8'));
+  return packageJson;
+};
+
+/**
+ * 工具函数，用来捕获并打印错误，返回false
+ */
+export const basicCatchError = (err: Error) => {
+  console.log(`\r\n ${chalk.red(err)}`);
+  return false;
+};
