ab70d9bd8c18408891a881734975e947c00fb4aa
lio-mengxiang <1334196450@qq.com>
2022-04-04 11:15:10 +0800

    feat: d

--- src/gitPush/index.ts
+++ src/gitPush/index.ts
@@ -21,6 +21,7 @@ export async function _gitPush() {
   }
   const curBranchName = run('git symbolic-ref --short HEAD');
   const getAllRemoteBranch =  run('git branch -r');
+  console.log('getAllRemoteBranch: ', getAllRemoteBranch);
   const isExistCurBranch =  run(`grep -w '${getAllRemoteBranch}'`);
   console.log('isExistCurBranch: ', isExistCurBranch);
   timeLog('准备推送代码至git仓库', 'start');
